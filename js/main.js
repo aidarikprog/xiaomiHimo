85f5b071c225ba4621ffd7db7dc79636dd139cba
aidarikprog <aidar1171995@mail.ru>
2022-06-21 17:59:51 +0500

    release 1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+    // это для карусели картинок
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 200,
+    })
+
+    // это для плавной прокрутки страницы
+    
+    $(".menu, .bike__column").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+})
